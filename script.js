3d26e155b8aa4b36f153359397f4d6ebe08babb2
Lucas Matheus Stangherlin <st.lucasm@gmail.com>
2022-08-02 11:18:42 -0300

    Refatoração caixas

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 function quem(){
     document.getElementById("conteudo_titulo").innerHTML = 'Quem é a Teacher Sabrina?';
-    document.getElementById("conteudo_descricao").innerHTML = '<p>Professora de inglês com especialização em língua inglesa pela Universidade Metodista, com anos e mais anos de\
-    experiência no ensino do idioma para turmas de todos os níveis, do mais básico ao avançado.</p>\
-\
-    <ul>\
-        <li class="experiencia">Access School - 2019 - Atual</li>\
-        <li class="experiencia">Professora Particular - 2018</li>\
-        <li class="experiencia">Influx English School - 2006 a 2011</li>\
-    </ul>'
+    document.getElementById("conteudo_descricao").innerHTML = '<p>Professora de inglês com especialização em língua inglesa pela Universidade Metodista, ensinando\
+o idioma em todos os níveis, do mais básico ao avançado desde 2006.</p>'
 
 }
 
