b954a838a4693a1e4042953f01d130021d418db6
Lucas Matheus Stangherlin <st.lucasm@gmail.com>
2022-08-02 13:46:14 -0300

    Adicionado conteúdo e animações nos botões

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-function quem(){
-    document.getElementById("conteudo_titulo").innerHTML = 'Quem é a Teacher Sabrina?';
-    document.getElementById("conteudo_descricao").innerHTML = '<p>Professora de inglês com especialização em língua inglesa pela Universidade Metodista, ensinando\
-o idioma em todos os níveis, do mais básico ao avançado desde 2006.</p>'
-
+function replace(element, content){
+document.getElementById(element).innerHTML = content
 }
 
-function pq(){
-    document.getElementById("conteudo_titulo").innerHTML = 'teste?'
-    document.getElementById("conteudo_descricao").innerHTML = '<p>teste</p>'
+var conteudo = "conteudo"
 
-}
+var who = '<h1 id="conteudo_titulo">Quem é a Teacher Sabrina?</h1>\
+<div id="conteudo_descricao">\
+<p>Radialista, Jornalista e Professora de inglês desde 2006. Possui especialização em língua inglesa pela Universidade Metodista (2009)\
+    e vem ensinando o idioma a alunos em todos os níveis, do básico ao avançado.</p>'
 
+var why = '<h1 id="conteudo_titulo">Por quê estudar inglês?</h1>\
+<div id="conteudo_descricao">\
+<p>\
+<ul>\
+    <li>O inglês é uma das línguas mais faladas no mundo todo, com mais de 400 milhões de falantes nativos, idioma oficial de mais de 50 países,\
+        e é praticamente um idioma universal que muitas pessoas aprendem como segundo idioma, abrindo a possibilidade de você se comunicar em\
+        qualquer lugar do mundo.</li>\
+    <li>O inglês é o idioma dos negócios e trará oportunidades para você na sua carreira, tornando seu currículo muito mais atrativo.</li>\
+    <li>A língua inglesa te dá acesso às melhores universidades do mundo</li>\
+    <li>A língua inglesa te dá a possibilidade de entender muito mais da cultura popular como séries, filmes, músicas, livros e vídeos </li>\
+    <li style="font-weight:bold">E muito, muito mais!</li>\
+</ul>\
+</p>'
+
+var what = '<h1 id="conteudo_titulo">O que você vai aprender no curso?</h1>\
+<div id="conteudo_descricao">\
+<p>O curso tem metodologia desevolvida pela propria teacher e vai se moldar conforme sua necessidade. Na primeira aula, vamos definir\
+quais são seus interesses e necessidades no idioma e, a partir disso, o curso vai seguir de forma adequada para seu nível de inglês\
+Vai aprender a gramática e estrutura da língua inglesa, vai poder praticar a leitura, a escrita e a conversação, sempre de forma\
+dinâmica, sem engessar o seu aprendizado </p>'
